3ace17631550be5df244e33d4d209b04d95a64d0
Ingi Kim <ikk5515@naver.com>
2022-01-03 16:19:54 +0900

    first commit

---
+++ variable.js
@@ -0,0 +1,13 @@
+//1. Use strict
+// added in ES 5
+// use this for Valina javascript
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+
+let name;
+let name = 'ingi';
+console.log(name);
+name = 'hello';
+console.log(name)
